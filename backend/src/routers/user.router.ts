eeb40567af9349ac1d21e759997725cfac7c7a2e
Ashish Narayan Shinde <ashishshinde765@gmail.com>
2022-12-02 13:00:29 +0530

    Initial commit

---
+++ backend/src/routers/user.router.ts
@@ -0,0 +1,79 @@
+import Router = require("express");
+import { sample_users } from "../data";
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import { User, UserModel } from "../models/user.model";
+import { HTTP_BAD_REQUEST } from "../constants/http_status";
+import bycrypt from 'bcryptjs';
+
+const router = Router();
+
+router.get("/seed",
+  asyncHandler(async (req, res) => {
+    const usersCount = await UserModel.countDocuments();
+    if (usersCount > 0) {
+      res.send("Seed is already done");
+      return;
+    }
+
+    await UserModel.create(sample_users);
+    res.send("Seed is done!");
+  })
+);
+
+router.post("/login", asyncHandler(
+  async(req: any, res: any) => {
+  const { email, password } = req.body;
+  let user = await UserModel.findOne({ email, password})
+
+  if (user) {
+    res.send(generateTokenResponse(user));
+  } else {
+    res.status(HTTP_BAD_REQUEST).send("user name or password is not valid");
+  }
+}
+));
+
+router.post("/register", asyncHandler(
+  async(req, res) => {
+    const { name, email, password, address} = req.body;
+    const user = await UserModel.findOne({email});
+    if(user){
+      res.status(HTTP_BAD_REQUEST).send("User is Alreadt exist, please do login");
+      return;
+    }
+    
+    const salt = bycrypt.genSaltSync(10)
+    const encryptedPassword = await bycrypt.hash(password, salt);
+
+    const newUser: User = {
+      id: "",
+      name,
+      email: email.toLowercase(),
+      password: encryptedPassword,
+      address,
+      isAdmin: false
+    };
+
+    const dbUser = await UserModel.create(newUser);
+    res.send(generateTokenResponse(dbUser))
+  }
+));
+
+const generateTokenResponse = (user: any) => {
+  const token = jwt.sign(
+    {
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    "SomeRandomText",
+    {
+      expiresIn: "30d",
+    }
+  );
+
+  user.token = token;
+  return user;
+};
+
+export default router;
